df53a734ce04e1fbe7e51bb8d5890c93c59cfbca
Sagar Mainali <sagarmainali78@gmail.com>
2023-05-02 15:39:29 +0545

    routes added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
-const App = () => { 
-     return <h1>This is App</h1>
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Home'
+import About from './About'
+
+const App = () => {
+
+     return <>
+          <BrowserRouter>
+               <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+               </Routes>
+          </BrowserRouter>
+     </>
 }
 
 export default App
