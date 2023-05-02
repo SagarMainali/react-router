c746e29f7cdb4ecb709b6b793757ddadda841808
Sagar Mainali <sagarmainali78@gmail.com>
2023-05-02 17:47:34 +0545

    using useParams

--- src/Pages/Details.tsx
+++ src/Pages/Details.tsx
@@ -1,5 +1,12 @@
+import { useParams } from "react-router-dom"
 
 function Details() {
+
+     // catching the name of the link that rendered this componenet
+     const params = useParams()
+
+     console.log(params)
+
      return (
           <h2>I am the children of about page</h2>
      )
