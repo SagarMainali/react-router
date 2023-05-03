1171f4016d44468bc39a045390593d2a3607ce77
Sagar Mainali <sagarmainali78@gmail.com>
2023-05-04 00:29:00 +0545

    fixed goback path

--- src/Components/HostVanDetailHeader.tsx
+++ src/Components/HostVanDetailHeader.tsx
@@ -2,8 +2,8 @@ import { NavLink } from "react-router-dom"
 
 function HostVanDetailHeader() {
      return (
-          <div>
-               <NavLink to='.'>Details</NavLink>
+          <div className="hostvandetailheader-links">
+               <NavLink to='.' end>Details</NavLink>
                <NavLink to='pricing'>Pricing</NavLink>
                <NavLink to='photos'>Photos</NavLink>
           </div>
--- src/Pages/Host/HostVanDetails.tsx
+++ src/Pages/Host/HostVanDetails.tsx
@@ -1,7 +1,7 @@
 import HostVanDetailHeader from "../../Components/HostVanDetailHeader"
 import useGetParams from "../../CustomHook/useGetParams"
-import useGoBack from "../../CustomHook/useGoBack"
-import { Outlet } from "react-router-dom"
+// import useGoBack from "../../CustomHook/useGoBack"
+import { NavLink, Outlet } from "react-router-dom"
 
 function HostVanDetails() {
 
@@ -9,7 +9,10 @@ function HostVanDetails() {
 
      return (
           <div>
-               <h4 onClick={useGoBack(-1)} style={{ textDecoration: 'underline', cursor: 'pointer' }}>Go back...</h4>
+               {/* this only works if only we are on the info page since it is a index page but if we navigate to pricing or phots. url gets added with this path
+               and when we go back we won't go to the hostvans page, instead we just go back from the current path*/}
+               {/* <h4 onClick={useGoBack(-1)} style={{ textDecoration: 'underline', cursor: 'pointer' }}>Go back...</h4> */}
+               <NavLink to='../hostvans' style={{ textDecoration: 'underline', cursor: 'pointer' }}>Go back...</NavLink>
                <h2>You are seeing the details of hostvan {param}</h2>
                <HostVanDetailHeader />
                <Outlet />
