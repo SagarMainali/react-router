af32dfc1fa0a91bdc6afc1366d6f2c59a50feab1
Sagar Mainali <sagarmainali78@gmail.com>
2023-05-04 00:47:39 +0545

    wrapping up

--- src/Pages/Host/HostVanDetails/HostVanDetailsInfo.tsx
+++ src/Pages/Host/HostVanDetails/HostVanDetailsInfo.tsx
@@ -3,6 +3,7 @@ import { useOutletContext } from "react-router-dom"
 export default function HostVanDetailsInfo() {
 
   const context: number = useOutletContext()
+
   return (
     <div>Info of hostvan {context}</div>
   )
